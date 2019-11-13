2a7ca29ac4bec62f3356ad5d70de2a4a07130316
Joseph Pritchett <joseph.pritchett@rgi-corp.local@localhost.localdomain>
2019-11-12 23:25:43 -0500

    Update scoring; correct damage inflicted logic

--- Entity.py
+++ Entity.py
@@ -13,7 +13,8 @@ class Entity(object):
         self._wounds = WoundManager(wounds=wounds, defense=defense)
         self._damage = damage
 
-        self._damage_inflicted = 0
+        self._damage_inflicted = dict()
+        self._wounds_inflicted = dict()
 
     @property
     def name(self) -> str:
@@ -25,7 +26,11 @@ class Entity(object):
 
     def _count_damage(self, target):
         print(f'{self.name} does {self._damage} damage to {target.name}')
-        self._damage_inflicted += self._damage
+        self._damage_inflicted[target.name] = self._damage_inflicted.setdefault(target.name, 0) + self._damage
+
+    def _count_wounds(self, target):
+        print(f'{self.name} wounds {target.name}')
+        self._wounds_inflicted[target.name] = self._wounds_inflicted.setdefault(target.name, 0) + 1
 
     def attack(self, target):
         # Keeping this simple for testing
@@ -35,8 +40,9 @@ class Entity(object):
         if result == ActionDice.RESULT.FAIL:
             print(f'{self.name} misses {target.name}')
             return  # Early exit
-        print(f'{self.name} hits {target.name}')
-        on_hit = lambda entity=target: self._count_damage(target=entity)  # This will be called when a hit causes damage
+
+        self._count_damage(target)
+        on_hit = lambda entity=target: self._count_wounds(target=entity)  # This will be called when a hit causes damage
         args = dict(damage=self._damage, on_hit=on_hit, crit=False)
         if result == ActionDice.RESULT.CRIT:
             args['crit'] = True
@@ -49,14 +55,14 @@ import random
 
 if __name__ == '__main__':
     player = Entity(wounds=4, defense=50, damage=15, name='Smackers')
-    monster1 = Entity(wounds=2, defense=35, damage=7)
-    monster2 = Entity(wounds=2, defense=35, damage=7)
+    monster1 = Entity(wounds=2, defense=35, damage=7, name='Fred')
+    monster2 = Entity(wounds=2, defense=35, damage=7, name='Bob')
 
     entities = [player, monster1, monster2]
     count = 1
     while player.wounds.active and (monster1.wounds.active or monster2.wounds.active):
         print(f'--- Round {count}---')
-        health_bars = [f'{_entity.name}: {_entity.wounds.wound_string}' for _entity in [player, monster1, monster2]]
+        health_bars = [f'{_entity.name}: {_entity.wounds.wound_string}' for _entity in entities]
         print(' '.join(health_bars))
         print()
         target = random.randint(0, 1)
@@ -67,4 +73,13 @@ if __name__ == '__main__':
         monster1.attack(player)
         monster2.attack(player)
         count += 1
+        print()
+
+    entity_map = {_entity.name: _entity for _entity in entities}
+    for _scorer in entities:
+        print(f'--- {_scorer.name} report:')
+        for _name, _damage in _scorer._damage_inflicted.items():
+            print(f'Did {_damage} damage to {_name}')
+        for _name, _wounds in _scorer._wounds_inflicted.items():
+            print(f'Wounded {_name} {_wounds} times')
         print()
